d40aa7fefd66d04bd7ff1e0eb299454b6ce876d8
kandalog <asuasu.tee@gmail.com>
2022-05-23 08:26:09 +0900

    books_basic詳細ページからのスクレイピングを実装

--- books_toscrape/books_toscrape/spiders/books_basic.py
+++ books_toscrape/books_toscrape/spiders/books_basic.py
@@ -12,12 +12,36 @@ class BooksBasicSpider(scrapy.Spider):
         urls = response.xpath('//h3/a/@href').getall()
         
         for i in range(len(titles)):
-            yield {
-                'title': titles[i],
-                'url': urls[i]
-            }
+            # yield {
+            #     'title': titles[i],
+            #     'url': urls[i]
+            # }
+
+            # yield response.followで詳細ページにアクセス
+            # 詳細ページ用のcallback関数を作成して渡す
+            yield response.follow(url=urls[i], callback=self.parse_item)
+        
+        # リンク
         next_page = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page:
             yield response.follow(url=next_page, callback=self.parse)
         
-        
+    
+    # callbackで渡す関数を定義
+    # 詳細ページの情報を引っ張ってくる
+    # response.xpath部分を直接yieldに渡しても良いかも
+    def parse_item(self, response):
+        title = response.xpath('//h1/text()').get()
+        price = response.xpath('//div[contains(@class, "product_main")]//p[@class="price_color"]/text()').get()
+        stock = response.xpath('//div[@class="col-sm-6 product_main"]//p[@class="instock availability"]/text()[2]').get()
+        rating = response.xpath('//p[contains(@class, "star-rating")]/@class').get()
+        upc = response.xpath('//th[contains(text(), "UPC")]/following-sibling::td/text()').get()
+        review = response.xpath('//th[contains(text(), "Number of reviews")]/following-sibling::td/text()').get()
+        yield {
+            'title': title,
+            'price': price,
+            'stock': stock,
+            'rating': rating,
+            'upc': upc,
+            'review': review
+        }
